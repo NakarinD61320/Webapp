f8c0a2b86e576219ce0da04f72c1670f0bcc50e4
NakarinD61320 <60766777+NakarinD61320@users.noreply.github.com>
2020-02-07 10:52:32 +0700

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Leb_6115247020.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
